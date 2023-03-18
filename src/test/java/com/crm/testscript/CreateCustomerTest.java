3f8500f9e499161eac6ced1100688b85be5f642c
vannurV <Vadde Vannur@TYSS-Vadde-Vannur>
2023-03-18 23:18:29 +0530

    commit

--- src/test/java/com/crm/testscript/CreateCustomerTest.java
+++ src/test/java/com/crm/testscript/CreateCustomerTest.java
@@ -2,21 +2,26 @@ package com.crm.testscript;
 
 
 import java.io.IOException;
+import java.util.Random;
+import java.util.stream.IntStream;
 
 import org.apache.poi.EncryptedDocumentException;
 import org.testng.Assert;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import com.crm.generic.BaseClass;
 import com.crm.generic.FileLib;
 import com.crm.objectrepository.Enter_Time_TrackPage;
 import com.crm.objectrepository.TaskListPage;
-
+@Listeners(com.crm.generic.ListenerImplementation.class)
 public class CreateCustomerTest extends BaseClass {
 	@Test
 	public void createCustomerTest() throws InterruptedException, EncryptedDocumentException, IOException {
 		FileLib fileLib=new FileLib();
-		
+		Random r=new Random();
+		int r1 = r.nextInt(1000);
+		String random=Integer.toString(r1);
 		String expectedCustomerName=fileLib.getExcelData("createCustomer", 1, 1);
 		String description=fileLib.getExcelData("createCustomer", 1, 2);
 		Enter_Time_TrackPage enter_Time_TrackPage=new Enter_Time_TrackPage(driver);
@@ -24,13 +29,13 @@ public class CreateCustomerTest extends BaseClass {
 		TaskListPage listPage=new TaskListPage(driver);
 		listPage.clickOnAddNewButton();
 		listPage.clickOnnewCustomerButton();
-		listPage.enterDataInCustomernameTextFiled(expectedCustomerName);
+		listPage.enterDataInCustomernameTextFiled(expectedCustomerName+random);
 		listPage.enterDataIncustomerDescriptionTextFiled(description);
-		listPage.clickOnDropDown();
-		listPage.selectOurCompany();
+		//listPage.clickOnDropDown();
+		//listPage.selectOurCompany();
 		listPage.clickOncreateCustomerButton();
 		String actualCustomerName = listPage.verifyCustomerName();
-		Assert.assertEquals(actualCustomerName, expectedCustomerName);	
+		Assert.assertEquals(actualCustomerName, expectedCustomerName+random);	
 	     fileLib.setExcelData("createCustomer", 1, 3, "pass");
 	}
 }
